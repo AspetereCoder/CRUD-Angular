c9c66d2fedc2eba9d834ffd9a7768b246baa530f
AspetereCoder <pedrinhoalgusto07@gmail.com>
2025-06-26 11:54:21 -0300

    adiciona pagina inicial de login

---
+++ src/app/components/button/button.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.scss'
+})
+export class ButtonComponent {
+  @Input() btnText: string;
+
+  ngOnInit() {
+    console.log("Componente inicializado");
+  }
+
+}
