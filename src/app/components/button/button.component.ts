e206f5606aa4a2c66e57c4017058cf53a0b31be3
AspetereCoder <pedrinhoalgusto07@gmail.com>
2025-07-07 22:27:49 -0300

    refatora identacao button.comp.ts

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -3,9 +3,8 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html',
-  styleUrl: './button.component.scss'
+  styleUrl: './button.component.scss',
 })
 export class ButtonComponent {
   @Input() btnText: string;
-
 }
