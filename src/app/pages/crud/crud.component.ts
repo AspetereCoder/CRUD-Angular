c55aa40dba63698ba875d0ba36c50b9a04404f85
AspetereCoder <pedrinhoalgusto07@gmail.com>
2025-07-07 22:24:52 -0300

    injeta serviço de usuarios na page crud
    lista os usuarios cadastrados na Realtime Database

--- src/app/pages/crud/crud.component.ts
+++ src/app/pages/crud/crud.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { User } from '../../interfaces/user';
+import { UsersService } from '../../services/users.service';
 
 @Component({
   selector: 'app-crud',
@@ -8,19 +9,34 @@ import { User } from '../../interfaces/user';
   styleUrl: './crud.component.scss',
 })
 export class CrudComponent {
-  dataSource: MatTableDataSource<User> = new MatTableDataSource([
-    { 
-      name: 'User1', // just a test user
-      email: 'testuser@email.com',
-      role: 'Dummy',
-      sector: 'Test',
-    },
-  ]);
+  dataSource: MatTableDataSource<User>;
   displayedColumns: string[] = ['name', 'role', 'email', 'benefits', 'actions'];
+
+  constructor(private usersService: UsersService) {}
+
+  ngOnInit() {
+    this.getUsersList();
+  }
+
+  getUsersList() {
+    return this.usersService.getAllUsers().subscribe({
+      next: (response: User[]) => {
+        // shows the users from firebase db on the screen
+        this.dataSource = new MatTableDataSource(response);
+      },
+      error: (err) => {
+        console.log("Erro:", err);
+      }
+    });
+  }
 
   // users filter
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
   }
 }
